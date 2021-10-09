55e86bd91eefc36dcebb26d5b6b5981a45daf0cf
nafisabdurrahmannugraha <77777954+nafisabdurrahmannugraha@users.noreply.github.com>
2021-10-09 13:56:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("helloworld");
+
+console.log("helloworld");
+
+function generate(){
+    total = prompt("How many hearts do you need?")
+    document.write("Nafis Abdurahaman" + total +"Anak Ganteng")
+    for (i=0; i<total; i++){
+      document.write("<p> &hearts; </p>")
+    }
+  }
